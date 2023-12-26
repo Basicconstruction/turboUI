bd48fcf6f8035ca8751a6d89d2d77c4cbfa982cd
betha <bethaniebell8474@gmail.com>
2023-12-26 11:18:52 +0800

    较为完备的主题切换

--- src/app/components/dialogue/markdown-root/copied-button/copied-button.component.ts
+++ src/app/components/dialogue/markdown-root/copied-button/copied-button.component.ts
@@ -1,8 +1,12 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 @Component({
   selector: 'app-copied-button',
   templateUrl: './copied-button.component.html',
+  imports: [
+    NzButtonModule
+  ],
   standalone: true
 })
 export class CopiedButtonComponent {
